68855b9afa327f9db99f20fcac44860c8140f5de
US\prituppalapati <prituppalapati@deloitte.com>
2018-05-07 11:21:38 +0530

    manifest service worker

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -261,46 +261,6 @@ self.addEventListener('fetch', function(event) {
 });
 
 
-self.addEventListener("push", function(event) {
-  console.log("Service Worker recived a push message", event.data.text());
-
-  var title = "New question posted. Click to open";
-  var content = event.data.text();
-  event.waitUntil(
-    self.registration.showNotification(title, {
-      body: JSON.parse(content).text,
-      data: JSON.parse(content).docId,
-      icon: "./assets/digigyan-48.png"
-    })
-  );
-});
-
-self.addEventListener("notificationclick", function(event) {
-  console.log("Notification click: tag", event.notification.tag);
-  event.notification.close();
-  var url =
-    "https://angular6-mean-pwa-boilerplate.herokuapp.com/#/article/" +
-    event.notification.data;
-  event.waitUntil(
-    clients
-      .matchAll({
-        type: "window"
-      })
-      .then(function(windowClients) {
-        console.log("WindowClients", windowClients);
-        for (var i = 0; i < windowClients.length; i++) {
-          var client = windowClients[i];
-          console.log("WindowClient", client);
-          if (client.url === url && "focus" in client) {
-            return client.focus();
-          }
-        }
-        if (clients.openWindow) {
-          return clients.openWindow(url);
-        }
-      })
-  );
-});
 
 
 
